efb13e8387f231560c5955f89041572fa64427a3
jerusalemmoore <jerusalemmoore@gmail.com>
2022-01-24 00:43:22 -0800

    testing 2

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome app!" />
+        <Header title="Welcome new app app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
